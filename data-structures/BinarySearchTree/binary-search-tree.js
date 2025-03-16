61b6869286cf38321707e747d6d494ec923d6103
h3vanth <rhevanthraj@gmail.com>
2025-03-16 22:23:05 +0530

    BinarySearchTree: Implement getNode, remove methods

--- data-structures/BinarySearchTree/binary-search-tree.js
+++ data-structures/BinarySearchTree/binary-search-tree.js
@@ -6,6 +6,7 @@ class TreeNode {
   }
 }
 
+// O(log n)
 class BinarySearchTree {
   constructor() {
     this.root = null;
@@ -13,36 +14,110 @@ class BinarySearchTree {
 
   insert(value) {
     const node = new TreeNode(value);
-    if (this.root == null) {
+    if (!this.root) {
       this.root = node;
-    } else {
-      let currentNode = this.root;
-      while (true) {
-        if (value < currentNode.value) {
-          if (!currentNode.left) {
-            currentNode.left = node;
-            return;
+      return;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = node;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = node;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  getNode(value) {
+    if (!this.root) {
+      return null;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (!currentNode) {
+        return null;
+      }
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  remove(value) {
+    if (!this.root) {
+      return null;
+    }
+
+    let prevNode = null;
+    let currentNode = this.root;
+    while (true) {
+      if (!currentNode) {
+        return null;
+      }
+      if (currentNode.value === value) {
+        if (!currentNode.left && !currentNode.right) {
+          if (prevNode.left === currentNode) {
+            prevNode.left = null;
+          } else {
+            prevNode.right = null;
           }
+        } else if (currentNode.left && !currentNode.right) {
+          if (value < prevNode.value) {
+            prevNode.left = currentNode.left;
+          } else {
+            prevNode.right = currentNode.left;
+          }
+        } else if (!currentNode.left && currentNode.right) {
+          if (value < prevNode.value) {
+            prevNode.left = currentNode.right;
+          } else {
+            prevNode.right = currentNode.right;
+          }
+        } else {
+          // Both left and right is present
+          const leftNode = currentNode.left;
+          const rightNode = currentNode.right;
+
+          prevNode.right = rightNode;
+          rightNode.left = leftNode;
+        }
+        return;
+      } else {
+        prevNode = currentNode;
+        if (value < currentNode.value) {
           currentNode = currentNode.left;
         } else {
-          if (!currentNode.right) {
-            currentNode.right = node;
-            return;
-          }
           currentNode = currentNode.right;
         }
       }
     }
   }
 
-  remove(value) {
-
-  }
-
-  _balance() {
-
-  }
+  _balance() {}
 }
+
+/*
+      1
+    /   \
+   0     2
+          \
+           7
+          / \
+         6   10
+*/
 
 function test() {
   const tree = new BinarySearchTree();
@@ -52,7 +127,14 @@ function test() {
   tree.insert(7);
   tree.insert(6);
   tree.insert(10);
-  console.log(tree);
+
+  tree.remove(7);
+
+  console.log(tree.getNode(1));
+  console.log(tree.getNode(0));
+  console.log(tree.getNode(2));
+  console.log(tree.getNode(6));
+  console.log(tree.getNode(10));
 }
 
-test()
+test();
